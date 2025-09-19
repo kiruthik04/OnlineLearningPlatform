40eedc26fd023b8c7d11b89e2eae6c701b08a169
kiruthik04 <kiruthicksathy@gmail.com>
2025-09-19 15:58:36 +0530

    AddNewCourse form submission is complete

--- app/workspace/_components/AddNewCourseDialog.jsx
+++ app/workspace/_components/AddNewCourseDialog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState  } from "react";
 import {
     Dialog,
     DialogContent,
@@ -18,8 +18,32 @@ import { Input } from "../../../@/components/ui/input";
 import { Textarea } from "../../../@/components/ui/textarea";
 import { Switch } from "../../../@/components/ui/switch"
 import { Button } from "../../../@/components/ui/button";
+import { SparkleIcon } from "lucide-react";
+import { desc } from "drizzle-orm";
 
 function AddNewCourseDialog({ children }) {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        description: '',
+        noOfChapter: 1,
+        includeVideo: false,
+        level: '',
+        category: ''
+    });
+
+    const onHandleInputChange = (field, value) => {
+        setFormData(pre => ({
+            ...pre,
+            [field]: value
+        }));
+        console.log(formData);
+    }
+
+    const onGenerate = () => {
+        console.log(formData);
+        // API call to generate course using AI
+    }
     return (
         <Dialog>
             <DialogTrigger asChild>{children}</DialogTrigger>
@@ -30,23 +54,27 @@ function AddNewCourseDialog({ children }) {
                         <div className='flex flex-col gap-3 mt-5'>
                             <div className='flex flex-col gap-1'>
                                 <label className="font-bold">Course Title</label>
-                                <Input placeholder="Enter course title" />
+                                <Input placeholder="Enter course title"
+                                    onChange={(event) => onHandleInputChange('name', event?.target.value)} />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label className="font-bold">Course Description (Optional)</label>
-                                <Textarea placeholder="Enter course description" />
+                                <Textarea placeholder="Enter course description"
+                                    onChange={(event) => onHandleInputChange('description', event?.target.value)} />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label className="font-bold">Number of Chapters</label>
-                                <Input placeholder="Enter number of chapters" type="number" />
+                                <Input placeholder="Enter number of chapters" type="number"
+                                    onChange={(event) => onHandleInputChange('noOfChapter', event?.target.value)} />
                             </div>
                             <div className="flex items-center gap-2">
                                 <label className="font-bold">Include Video:</label>
-                                <Switch />
+                                <Switch
+                                    onCheckedChange={() => onHandleInputChange('includeVideo', !formData?.includeVideo)} />
                             </div>
                             <div className="flex items-center gap-2">
                                 <label className="font-bold">Difficulty Level:</label>
-                                <Select>
+                                <Select onValueChange={(value) => onHandleInputChange('level', value)}>
                                     <SelectTrigger className="w-[180px]">
                                         <SelectValue placeholder="Difficulty Level" />
                                     </SelectTrigger>
@@ -59,16 +87,13 @@ function AddNewCourseDialog({ children }) {
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label className="font-bold">Category:</label>
-                                <Input placeholder="Eg: Technology, Business" />
-                            </div>
-                            <div className='flex flex-col gap-1'>
-                                <label className="font-bold">Target Audience:</label>
-                                <Textarea placeholder="Who is this course for?" />
+                                <Input placeholder="Eg: Technology, Business"
+                                    onChange={(event) => onHandleInputChange('category', event?.target.value)} />
                             </div>
                         </div>
                     </DialogDescription>
                 </DialogHeader>
-                <Button className="mt-4">Create Course</Button>
+                <Button className="mt-5" onClick={onGenerate}> <SparkleIcon /> Create Course</Button>
             </DialogContent>
         </Dialog >
     )
